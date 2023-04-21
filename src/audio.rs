f0cabf6c2b241eac5c0202fc9ebf51283bfb294a
mjzilver <mjzilver@avans.nl>
2023-04-21 20:42:33 +0200

    save decoded sound-effects to memory

--- src/audio.rs
+++ src/audio.rs
@@ -1,28 +1,49 @@
+use std::fs::File;
 use std::io::BufReader;
+use std::collections::HashMap;
 
-use rodio::{OutputStreamHandle, OutputStream};
+use rodio::source::{Buffered, SamplesConverter};
+use rodio::{Decoder, OutputStream, OutputStreamHandle, Source, Sink};
 
 pub struct Audio {
     stream_handle: OutputStreamHandle,
-    _stream: OutputStream
+    _stream: OutputStream,
+    audio_cache: HashMap<String, Buffered<Decoder<BufReader<File>>>>,
 }
 
 impl Audio {
     pub fn new() -> Audio {
         let (_stream, stream_handle) = rodio::OutputStream::try_default().unwrap();
 
-        Audio {
+
+        let mut audio = Audio {
             stream_handle,
             _stream,
-        }
+            audio_cache: HashMap::new(),
+        };
+
+        audio.load_file("lose");
+        audio.load_file("blip");
+        audio.load_file("woosh");
+
+        audio
     }
 
-    pub fn play_audio(&self, name: &str) {
+    fn load_file(&mut self, name: &str) {
         let filename = format!("assets/{}.mp3", name);
-        let file = std::fs::File::open(filename).unwrap();
-     
-        let sink = self.stream_handle.play_once(BufReader::new(file)).unwrap();
-        sink.set_volume(1.0);
-        sink.detach();
+        let value =  Decoder::new(BufReader::new(File::open(filename).expect("file not found")))
+        .expect("could not load source")
+        .buffered();
+
+        self.audio_cache.insert(name.to_owned(), value);
+    }
+
+    pub fn play_audio(&mut self, name: &str) {
+        let buffer: SamplesConverter<Buffered<Decoder<BufReader<File>>>, i16> = self.audio_cache.get(name).unwrap().clone().convert_samples();
+        let sink = Sink::try_new(&self.stream_handle).unwrap();
+        sink.set_volume(0.8);
+        sink.append(buffer);
+        sink.play();
+        sink.detach()
     }
 }
